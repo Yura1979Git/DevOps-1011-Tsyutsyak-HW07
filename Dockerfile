f6e49f583962b88120cbb20f93bbc5126ba23ddc
Yura1979Git <tyi+git@outlook.com>
2023-01-16 12:41:44 +0200

    Add docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
 FROM python:3.8-alpine3.16
 
+RUN adduser --disabled-password webapp
+
+USER webapp
+
 WORKDIR /app
 
 EXPOSE 5000
 
 COPY ["requirements.txt", "./"]
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements.txt --no-cache-dir
 
 COPY ["app.py", "./"]
 
-CMD [ "python3", "./app.py"]
+ENTRYPOINT [ "python3" ]
+
+CMD [ "./app.py"]
--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-#redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    #redis.incr('hits')
-    #counter = str(redis.get('hits'), 'utf-8')
-    counter = "1"
+    redis.incr('hits')
+    counter = str(redis.get('hits'), 'utf-8')
     return "Welcome to our Flask app with Redis!, This web page was withites " + counter + " times"
 
 if __name__ == '__main__':
